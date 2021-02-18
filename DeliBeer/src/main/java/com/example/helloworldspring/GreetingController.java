3de2dd8212248a63717d2beb4826de27a0d74a3f
Isaactides <isaactides@gmail.com>
2021-02-18 10:47:28 +0100

    Create GreetingController.java

---
+++ DeliBeer/src/main/java/com/example/helloworldspring/GreetingController.java
@@ -0,0 +1,22 @@
+package com.example.helloworldspring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class GreetingController {
+	
+	@GetMapping("/hola")
+	public String hola() {
+		return "hola.html";
+	}
+	
+	@GetMapping("/greeting")
+	public String greeting(Model model) {
+		model.addAttribute("name", "World");
+		
+		return "greeting_template";
+	}
+	
+}

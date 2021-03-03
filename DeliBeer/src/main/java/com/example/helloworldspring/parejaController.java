d89150dc1ca8f2a24b953c1c1c8845ecfcd12196
IvanLopez1997 <60182691+IvanLopez1997@users.noreply.github.com>
2021-03-03 23:13:20 +0100

    Update parejaController.java

--- DeliBeer/src/main/java/com/example/helloworldspring/parejaController.java
+++ DeliBeer/src/main/java/com/example/helloworldspring/parejaController.java
@@ -8,11 +8,34 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class parejaController {
 	
+	@Autowired
+	private ParejaRepository packs;
 	
 	@GetMapping("/pareja")
 	public String pareja(Model model) {
 		
+		PackCerveza pack = packs.findByNombre("pareja");
+		List<Comentarios> comentarios = pack.getComentarios();
+		model.addAtribute("comentarios", comentarios);
 		return "pareja";
+	}
+	
+	@GetMapping("/inicial")
+	public String inicial(Model model) {
+		
+		return "inicial";
+	}
+	
+	@GetMapping("/profesional")
+	public String profesional(Model model) {
+		
+		return "profesional";
+	}
+	
+	@GetMapping("/fiesta")
+	public String fiesta(Model model) {
+		
+		return "fiesta";
 	}
 
 }

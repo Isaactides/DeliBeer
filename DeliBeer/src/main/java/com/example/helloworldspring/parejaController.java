2f7be39c1a2ccb2031cb5433fffcdb932cc56d82
Isaactides <isaactides@gmail.com>
2021-03-03 22:52:54 +0100

    Update parejaController.java

--- DeliBeer/src/main/java/com/example/helloworldspring/parejaController.java
+++ DeliBeer/src/main/java/com/example/helloworldspring/parejaController.java
@@ -1,5 +1,8 @@
 package com.example.helloworldspring;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,14 +17,20 @@ public class parejaController {
 	@GetMapping("/pareja")
 	public String pareja(Model model) {
 		
-		PackCerveza pack = packs.findByNombre("pareja");
-		List<Comentarios> comentarios = pack.getComentarios();
-		model.addAtribute("comentarios", comentarios);
+		List<PackCerveza> pack = packs.findByNombre("pareja");
+		PackCerveza cerve = pack.get(0);
+		List<Comentario> comentarios = cerve.getComentarios();
+		model.addAttribute("comentarios", comentarios);
 		return "pareja";
 	}
 	
 	@GetMapping("/inicial")
 	public String inicial(Model model) {
+		
+		List<PackCerveza> pack = packs.findByNombre("inicial");
+		PackCerveza cerve = pack.get(0);
+		List<Comentario> comentarios = cerve.getComentarios();
+		model.addAttribute("comentarios", comentarios);
 		
 		return "inicial";
 	}
@@ -29,11 +38,21 @@ public class parejaController {
 	@GetMapping("/profesional")
 	public String profesional(Model model) {
 		
+		List<PackCerveza> pack = packs.findByNombre("profesional");
+		PackCerveza cerve = pack.get(0);
+		List<Comentario> comentarios = cerve.getComentarios();
+		model.addAttribute("comentarios", comentarios);
+		
 		return "profesional";
 	}
 	
 	@GetMapping("/fiesta")
 	public String fiesta(Model model) {
+		
+		List<PackCerveza> pack = packs.findByNombre("fiesta");
+		PackCerveza cerve = pack.get(0);
+		List<Comentario> comentarios = cerve.getComentarios();
+		model.addAttribute("comentarios", comentarios);
 		
 		return "fiesta";
 	}

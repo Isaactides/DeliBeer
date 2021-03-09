64fb3ce4a9d6aef8c15bc01699af6ed24e06b332
Isaactides <isaactides@gmail.com>
2021-03-09 11:00:48 +0100

    Update PedidoController.java

--- DeliBeer/src/main/java/com/example/helloworldspring/PedidoController.java
+++ DeliBeer/src/main/java/com/example/helloworldspring/PedidoController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -19,43 +20,18 @@ import org.springframework.web.bind.annotation.RestController;
 import static org.springframework.web.servlet.support.ServletUriComponentsBuilder.fromCurrentRequest;
 
 
-@RestController
 @RequestMapping("/pedidos")
 @Controller
 public class PedidoController {
 	@Autowired
 	private PedidoRepository pedidos;
 	
-	@GetMapping("/")
-	public Collection<Pedido> getPedidos() {
-		return pedidos.findAll();
-	}
 	
-	@GetMapping("/{id}")
-	public Pedido getPedidos(@PathVariable long id){
-		
-		return pedidos.findById(id).orElseThrow();	
-	}
-	
-	
-	
-	@PostMapping("/pedido/new")
-	public String newPedido(Model model, Pedido pedido) {
+	@PostMapping("/pedidoaux")
+	public String nuevoPedido(Model model, Pedido pedido) {
 		pedidos.save(pedido);
-		return "savepedido";
+		model.addAttribute("pedido", pedido);
+		return "comprado";
 	}
 	
-	
-	@PutMapping("/{id}")
-	public Pedido replacePedido(@PathVariable long id,
-			@RequestBody Pedido newPedido) {
-		
-		Pedido pedido = pedidos.findById(id).orElseThrow();
-		
-		newPedido.setId(id);
-				
-		pedidos.save(newPedido);
-		
-		return newPedido;
-	}
 }

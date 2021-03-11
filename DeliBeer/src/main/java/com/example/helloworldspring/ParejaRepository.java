040230ae08c0a6f72859cc3fc417392b5b03c010
IvanLopez1997 <60182691+IvanLopez1997@users.noreply.github.com>
2021-03-11 13:53:52 +0100

    Update ParejaRepository.java

--- DeliBeer/src/main/java/com/example/helloworldspring/ParejaRepository.java
+++ DeliBeer/src/main/java/com/example/helloworldspring/ParejaRepository.java
@@ -7,8 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface ParejaRepository extends JpaRepository <PackCerveza, Long>{
 	
 	List<PackCerveza> findByNombre(String nombre);
-	
-	
+
 	
 	
 	

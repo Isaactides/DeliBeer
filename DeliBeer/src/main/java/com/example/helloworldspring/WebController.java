eba42588a6cbe9d77344b639d90ca7218a579575
Isaactides <isaactides@gmail.com>
2021-03-25 11:51:14 +0100

    Update WebController.java

--- DeliBeer/src/main/java/com/example/helloworldspring/WebController.java
+++ DeliBeer/src/main/java/com/example/helloworldspring/WebController.java
@@ -1,11 +1,22 @@
 package com.example.helloworldspring;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 
 
 @Controller
 public class WebController {
+	@RequestMapping("/login")
+	public String home(Model model, HttpServletRequest request) {
+		model.addAttribute("admin", request.isUserInRole("ADMIN"));
+		model.addAttribute("user", request.isUserInRole("USER"));
+		return "login";
+	}
 
 	@GetMapping("/login")
 	public String login() {

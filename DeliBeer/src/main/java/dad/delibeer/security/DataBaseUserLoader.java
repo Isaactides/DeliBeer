a40db8c0b9d64291b3bf66760ff68dbe324a6f76
Isaactides <isaactides@gmail.com>
2021-04-20 13:14:23 +0200

    Create DataBaseUserLoader.java

---
+++ DeliBeer/src/main/java/dad/delibeer/security/DataBaseUserLoader.java
@@ -0,0 +1,24 @@
+package dad.delibeer.security;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import dad.delibeer.model.User;
+import dad.delibeer.repositories.UserRepository;
+
+@Component
+public class DataBaseUserLoader {
+	@Autowired
+	private UserRepository userRepository;
+	
+	@PostConstruct
+	private void initDataBase() {
+		
+		userRepository.save(new User("user", "pass", "user@user.com", "ROLE_USER"));
+		userRepository.save(new User("admin", "adminpass","admin@admin.com", "ROLE_ADMIN"));
+	}
+	
+}

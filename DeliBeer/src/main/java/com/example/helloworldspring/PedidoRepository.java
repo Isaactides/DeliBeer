0f491d7ca08c5103199d5e7e50eb04b3391fdcdc
Isaactides <isaactides@gmail.com>
2021-04-20 13:13:03 +0200

    Delete PedidoRepository.java

--- DeliBeer/src/main/java/com/example/helloworldspring/PedidoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.helloworldspring;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.helloworldspring.Pedido;
-
-public interface PedidoRepository extends JpaRepository<Pedido, Long>{
-	
-}

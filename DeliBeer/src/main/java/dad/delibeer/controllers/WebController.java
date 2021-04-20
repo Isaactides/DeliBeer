5f11eb202155e7aafdc75d000f20e176d134d35d
Isaactides <isaactides@gmail.com>
2021-04-20 13:13:53 +0200

    Create WebController.java

---
+++ DeliBeer/src/main/java/dad/delibeer/controllers/WebController.java
@@ -0,0 +1,36 @@
+package dad.delibeer.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+
+@Controller
+public class WebController {
+	@RequestMapping("/login")
+	public String home(Model model, HttpServletRequest request) {
+		model.addAttribute("admin", request.isUserInRole("ADMIN"));
+		model.addAttribute("user", request.isUserInRole("USER"));
+		return "login";
+	}
+
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+
+	@GetMapping("/loginerror")
+	public String loginerror() {
+		return "loginerror";
+	}
+
+	@GetMapping("/home")
+	public String home() {
+		return "home";
+	}
+	
+}

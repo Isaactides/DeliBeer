8b3064141ba5f3060e66f7a7dba19f1304c72210
Isaactides <isaactides@gmail.com>
2021-04-20 13:13:55 +0200

    Create DeliBeerApplication.java

---
+++ DeliBeer/src/main/java/dad/delibeer/DeliBeerApplication.java
@@ -0,0 +1,13 @@
+package dad.delibeer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliBeerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliBeerApplication.class, args);
+	}
+
+}

3a6acbb3c1108e13d5dae90b585f791122258019
Isaactides <isaactides@gmail.com>
2021-04-20 13:12:21 +0200

    Delete ComentarioRepository.java

--- DeliBeer/src/main/java/com/example/helloworldspring/ComentarioRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.helloworldspring;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ComentarioRepository extends JpaRepository<Comentario, Long>{
-
-}

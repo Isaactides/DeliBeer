b3fce283c57f69a2824e39b125ac95884bfa20b9
Isaactides <isaactides@gmail.com>
2021-04-26 20:35:15 +0200

    Update ServicioInterno.java

--- DeliBeer/src/main/java/dad/delibeer/controllers/ServicioInterno.java
+++ DeliBeer/src/main/java/dad/delibeer/controllers/ServicioInterno.java
@@ -1,18 +1,19 @@
 package dad.delibeer.controllers;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
 
-import dad.delibeer.model.Pedido;
 
 public class ServicioInterno {
 	public ServicioInterno() {}
 
-	public void enviar(Pedido pedido, String mail) {
+	public void enviar(String nombre, String apellido, String tipo, Double precio, String mail) {
 		Socket cliente;
 		int puerto = 9000;
 		String ip = "127.0.0.1";
@@ -20,25 +21,23 @@ public class ServicioInterno {
 		try {
 			cliente = new Socket(ip, puerto);
 			OutputStream os = cliente.getOutputStream();
-			//ObjectOutputStream oos = new ObjectOutputStream(os);
-			DataOutputStream dataOutputStream = new DataOutputStream(os);
-			DataOutputStream dataMail = new DataOutputStream(os);
+//			OutputStreamWriter osw = new OutputStreamWriter(os);
+//            BufferedWriter bw = new BufferedWriter(osw);
+			ObjectOutputStream oos = new ObjectOutputStream(os);
 			//BufferedReader reader = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
 			
-			String nombreAux = pedido.getNombre();
-			String apellidoAux = pedido.getApellidos();
-			String tipoPedidoAux = pedido.getTipo_pedido();
-			Double precioAux = pedido.getPrecio_pedido();
+//			String nombreAux = pedido.getNombre();
+//			String apellidoAux = pedido.getApellidos();
+//			String tipoPedidoAux = pedido.getTipo_pedido();
+//			Double precioAux = pedido.getPrecio_pedido();
 			
-			String total = "Datos de su Pedido en DeliBeer: \n" + "Pedido realizado por: " + pedido.getNombre() + " " + pedido.getApellidos() + "\n Tipo de pedido: " + pedido.getTipo_pedido() + "\n Precio total: " + pedido.getPrecio_pedido();
+			String total = "Datos de su Pedido en DeliBeer: \n" + "Pedido realizado por: " + nombre + " " + apellido + "\n Tipo de pedido: " + tipo + "\n Precio total: " + precio;
 			
-			dataOutputStream.writeUTF(total);
-			dataOutputStream.flush();
-			dataOutputStream.close();
+			String[] array = {total,mail};
+//			bw.write(total);
+//			bw.flush();
+//			bw.close();
 			
-			dataMail.writeUTF(mail);
-			dataMail.flush();
-			dataMail.close();
 			
 			/*
 			oos.writeUTF(pedido.getNombre());
@@ -50,27 +49,13 @@ public class ServicioInterno {
 			oos.writeObject(apellidoAux);
 			oos.writeObject(tipoPedidoAux);
 			oos.writeObject(precioAux);
-			
-			oos.writeObject(mail);
-			oos.close();
 			*/
+			oos.writeObject(array);
+			oos.close();
+			
 			cliente.close();
 			os.close();
 		} catch (Exception e) {}
 	}
 	
-	public String recibir() {
-		Socket cliente;
-		int puerto = 9000;
-		String ip = "127.0.0.1";
-		String url=null;
-		try {
-			cliente = new Socket(ip, puerto);
-			BufferedReader reader = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
-			url = reader.readLine();
-			cliente.close();
-		} catch (Exception e) {}
-		System.out.println(url);
-		return url;
-	}
 }

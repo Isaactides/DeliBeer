b71aaf71c54ce66400d5fb77b633e489e7a3d848
IvanLopez1997 <60182691+IvanLopez1997@users.noreply.github.com>
2021-04-26 14:04:50 +0200

    Update ServicioInterno.java

--- DeliBeer/src/main/java/dad/delibeer/controllers/ServicioInterno.java
+++ DeliBeer/src/main/java/dad/delibeer/controllers/ServicioInterno.java
@@ -1,6 +1,7 @@
 package dad.delibeer.controllers;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
@@ -19,14 +20,40 @@ public class ServicioInterno {
 		try {
 			cliente = new Socket(ip, puerto);
 			OutputStream os = cliente.getOutputStream();
-			ObjectOutputStream oos = new ObjectOutputStream(os);
-			BufferedReader reader = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
+			//ObjectOutputStream oos = new ObjectOutputStream(os);
+			DataOutputStream dataOutputStream = new DataOutputStream(os);
+			DataOutputStream dataMail = new DataOutputStream(os);
+			//BufferedReader reader = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
 			
+			String nombreAux = pedido.getNombre();
+			String apellidoAux = pedido.getApellidos();
+			String tipoPedidoAux = pedido.getTipo_pedido();
+			Double precioAux = pedido.getPrecio_pedido();
 			
+			String total = "Datos de su Pedido en DeliBeer: \n" + "Pedido realizado por: " + pedido.getNombre() + " " + pedido.getApellidos() + "\n Tipo de pedido: " + pedido.getTipo_pedido() + "\n Precio total: " + pedido.getPrecio_pedido();
 			
-			oos.writeObject(pedido);
+			dataOutputStream.writeUTF(total);
+			dataOutputStream.flush();
+			dataOutputStream.close();
+			
+			dataMail.writeUTF(mail);
+			dataMail.flush();
+			dataMail.close();
+			
+			/*
+			oos.writeUTF(pedido.getNombre());
+			oos.writeUTF(pedido.getApellidos());
+			oos.writeUTF(pedido.getTipo_pedido());
+			oos.writeDouble(pedido.getPrecio_pedido());
+			
+			oos.writeObject(nombreAux);
+			oos.writeObject(apellidoAux);
+			oos.writeObject(tipoPedidoAux);
+			oos.writeObject(precioAux);
+			
 			oos.writeObject(mail);
 			oos.close();
+			*/
 			cliente.close();
 			os.close();
 		} catch (Exception e) {}

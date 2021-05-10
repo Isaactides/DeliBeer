85d8805b862628633be87e006b7e34f3df6322f2
colliga99 <angeleitorcolliga@gmail.com>
2021-05-10 14:21:31 +0200

    Update DeliBeerApplication.java

--- DeliBeer/src/main/java/dad/delibeer/DeliBeerApplication.java
+++ DeliBeer/src/main/java/dad/delibeer/DeliBeerApplication.java
@@ -1,13 +1,25 @@
 package dad.delibeer;
 
+import org.apache.commons.logging.Log;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.apache.commons.logging.LogFactory;
 
 @SpringBootApplication
 public class DeliBeerApplication {
 
+	private static final Log LOG=LogFactory.getLog(DeliBeerApplication.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DeliBeerApplication.class, args);
 	}
-
+	
+	@Bean
+	public CacheManager cacheManager() {
+		LOG.info("activating cache...");
+		return new ConcurrentMapCacheManager("packs");
+	}
 }

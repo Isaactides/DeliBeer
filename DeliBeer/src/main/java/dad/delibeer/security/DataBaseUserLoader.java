2eb428325894bd1d0ea8dbc75d6aa43cea36ec4f
IvanLopez1997 <60182691+IvanLopez1997@users.noreply.github.com>
2021-06-22 11:52:40 +0200

    Update DataBaseUserLoader.java

--- DeliBeer/src/main/java/dad/delibeer/security/DataBaseUserLoader.java
+++ DeliBeer/src/main/java/dad/delibeer/security/DataBaseUserLoader.java
@@ -11,19 +11,15 @@ import dad.delibeer.repositories.UserRepository;
 
 @Component
 public class DataBaseUserLoader {
+	
 	@Autowired
 	private UserRepository userRepository;
 	
 	@PostConstruct
 	private void initDataBase() {
 		
-		if (userRepository.findByName("user") != null) {}
-		else {
-		userRepository.save(new User("user", "pass", "user@user.com", "ROLE_USER"));}
-		
-		if (userRepository.findByName("admin") != null) {}
-		else {
-		userRepository.save(new User("admin", "adminpass","admin@admin.com", "ROLE_ADMIN"));}
+		userRepository.save(new User("user", "pass", "user@user.com", "ROLE_USER"));
+		userRepository.save(new User("admin", "adminpass","admin@admin.com", "ROLE_ADMIN"));
 	}
 	
 }

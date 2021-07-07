d796e4af790f9341f47e3927d03904b9ab1af715
IvanLopez1997 <60182691+IvanLopez1997@users.noreply.github.com>
2021-07-07 11:46:54 +0200

    Update DeliBeerApplication.java

--- DeliBeer/src/main/java/dad/delibeer/DeliBeerApplication.java
+++ DeliBeer/src/main/java/dad/delibeer/DeliBeerApplication.java
@@ -1,12 +1,21 @@
 package dad.delibeer;
 
+
 import org.apache.commons.logging.Log;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.apache.commons.logging.LogFactory;
+
+import org.springframework.session.hazelcast.config.annotation.web.http.EnableHazelcastHttpSession;
+import com.hazelcast.config.Config;
+import com.hazelcast.config.JoinConfig;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class DeliBeerApplication {
@@ -22,4 +31,22 @@ public class DeliBeerApplication {
 		LOG.info("activating cache...");
 		return new ConcurrentMapCacheManager("packs");
 	}
+	
+	
+	@Bean
+	 public Config config() {
+	 Config config = new Config();
+	 JoinConfig joinConfig = config.getNetworkConfig().getJoin();
+	 joinConfig.getMulticastConfig().setEnabled(false);
+	 
+	 List<String> serversList = new ArrayList<String>();
+    serversList.add("delibeer1");
+    serversList.add("delibeer2");
+	 
+	 joinConfig.getTcpIpConfig().setEnabled(true).setMembers(serversList);
+	 return config;
+	}
+	
+	
 }
+
